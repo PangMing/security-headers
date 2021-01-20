9658e9350d85a22fe27b1246d050423cc449502b
PangMing <pym1018@gmail.com>
2021-01-20 12:43:42 +0800

    Create worker.js

---
+++ worker.js
@@ -0,0 +1,41 @@
+const securityHeaders = {
+        "Content-Security-Policy": "upgrade-insecure-requests",
+        "X-Xss-Protection": "1; mode=block",
+        "X-Frame-Options": "SAMEORIGIN",
+        "X-Content-Type-Options": "nosniff",
+        "Referrer-Policy": "strict-origin-when-cross-origin"
+    },
+    sanitiseHeaders = {
+        Server: ""
+    },
+    removeHeaders = [
+        "Public-Key-Pins",
+        "X-Powered-By",
+        "X-AspNet-Version"
+    ];
+
+async function addHeaders(req) {
+    const response = await fetch(req),
+        newHeaders = new Headers(response.headers),
+        setHeaders = Object.assign({}, securityHeaders, sanitiseHeaders);
+
+    if (newHeaders.has("Content-Type") && !newHeaders.get("Content-Type").includes("text/html")) {
+        return new Response(response.body, {
+            status: response.status,
+            statusText: response.statusText,
+            headers: newHeaders
+        });
+    }
+
+    Object.keys(setHeaders).forEach(name => newHeaders.set(name, setHeaders[name]));
+
+    removeHeaders.forEach(name => newHeaders.delete(name));
+
+    return new Response(response.body, {
+        status: response.status,
+        statusText: response.statusText,
+        headers: newHeaders
+    });
+}
+
+addEventListener("fetch", event => event.respondWith(addHeaders(event.request)));
